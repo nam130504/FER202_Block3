a5f9fb8be3ada75514bb6a454f4e4bd33f2b8907
nam130504 <namnvde180028@fpt.edu.vn>
2025-08-11 10:36:13 +0700

    Update code

---
+++ slot1/slot1-ex1/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  const myName = "Nguyen Van Nam"; 
+
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <p>My name is Nam {myName}</p>
+    </div>
+  );
+}
+
+export default App;
